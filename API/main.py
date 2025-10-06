a33776d0d1bcc639d7f217b7c71958efcb4a21b7
vivekjangid17 <vj833930@gmail.com>
2025-10-06 05:52:56 +0530

    update FastAPI code

--- API/main.py
+++ API/main.py
@@ -1,15 +1,28 @@
 from fastapi import FastAPI, UploadFile, File
+import uvicorn
+from io import BytesIO
+from PIL import Image
+import numpy as np
 
 app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Welcome to the Potato Disease Detection API!"}
 
 @app.get("/ping")
 async def ping():
     return "Hello, I am alive."
 
+def read_file_as_image(data) -> np.ndarray:
+    image = np.array(Image.open(BytesIO(data)))
+    return image
+
 @app.post("/predict")
 async def predict(
     file: UploadFile = File(...)
 ):
+    image = read_file_as_image(await file.read())
     pass
 
 
